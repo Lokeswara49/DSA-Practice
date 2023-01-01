46d1c6a23915af6b36b3cb0a0a5937e7b3d7227f
Lokeswara49 <121753810+Lokeswara49@users.noreply.github.com>
2023-01-01 21:54:08 +0530

    Added main method and called the  function.

--- Arrays/1.Pascal Triangle.cpp
+++ Arrays/1.Pascal Triangle.cpp
@@ -1,9 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
         ans.push_back({1});
-        //ans.push_back({1,1});
         for(int i=1;i<numRows;i++){
             vector<int> temp;
             temp.push_back(1);
@@ -16,3 +17,16 @@ public:
         return ans;
     }
 };
+int main(){
+    int numRows;
+    cin>>numRows;
+    Solution obj;
+    vector<vector<int>> ans=obj.generate(numRows);
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
